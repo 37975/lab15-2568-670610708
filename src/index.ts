e5c1504ffc2888aaf8d09d5c6bd418ae5fff17c2
TIM <86812783+Theejuth@users.noreply.github.com>
2025-09-26 12:14:44 +0700

    COMPLETE!

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
 import morgan from 'morgan';
+import { success } from "zod";
+import { courses, students } from "./db/db.js";
+import studentRoutes from "./routes/studentRoutes.js";
+import courseRoutes from "./routes/courseRoutes.js";
 
 const app: any = express();
 
 //Middleware
 app.use(express.json());
 app.use(morgan('dev'));
+
+app.get("/", (req: Request, res: Response) => {
+  return res.status(200).json({
+    success: true,
+    message: "lab 15 API service successfully"
+  });
+});
+
+app.use("/me", studentRoutes);
+
+app.use("/api/v2", courseRoutes);
 
 app.listen(3000, () =>
   console.log("🚀 Server running on http://localhost:3000")
--- src/routes/studentRoutes.ts
+++ src/routes/studentRoutes.ts
@@ -1,7 +1,19 @@
-import { Router } from "express";
+import { Router, type Request, type Response } from "express";
+import { success } from "zod";
 const router = Router();
 
-router.get("/",() => {
+router.get("/",(req: Request, res: Response) => {
+    return res.json({
+        success: true,
+        message: "Student Information",
+        data: {
+            studentId: "670610708",
+            firstName: "Teejutha",
+            lastName: "Konsombat",
+            program: "CPE",
+            section: "001",
+        }
+    });
 });
 
 export default router;
